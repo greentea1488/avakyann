28c1286b4cd4bbf059ce7402f9abb8a226dbf1fa
greentea1488 <84225678+greentea1488@users.noreply.github.com>
2021-05-15 12:14:09 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public void met()
+        {
+            int n = 30, m = 60;
+            int sum = 0;
+            for (int i = n; i <= m; i++)
+                if (i % 3 == 0)
+                {
+
+                    sum += i;
+                }
+            Console.WriteLine("Сумма чисел кратным 3м в диапазоне от 30 до 60: "+sum);
+        }
+    }
+            
+        
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.met();
+
+        }
+    }
+}
